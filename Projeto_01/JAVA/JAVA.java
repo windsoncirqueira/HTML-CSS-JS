83723a596dd25b169615e14d732f3513eaa38d14
Windson Ciequeira dos Santos <109114778+windsoncirqueira@users.noreply.github.com>
2022-07-11 20:37:49 -0300

    Initial commit

---
+++ Projeto_01/JAVA/JAVA.java
@@ -0,0 +1,10 @@
+public class hello
+{
+    /**
+    * Meu primeiro programa em java!
+    */
+    public static vold main(string[] arg){
+        //Mostra na tela o texto entre "..."
+        sytem.out.println("Este é nosso primeiro programa")
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# HTML-CSS-JS
+ Estudo de front end
